eeb184609f847b5c1d73108a243531817b7e9474
Likon69 <50536335+Likon69@users.noreply.github.com>
2026-05-21 00:28:45 +0200

    Initial commit — ProfessionBuddy WotLK 3.3.5a port

---
+++ Updater.cs
@@ -0,0 +1,10 @@
+﻿namespace HighVoltz
+{
+    public static class Updater
+    {
+        public static void CheckForUpdate()
+        {
+            // SVN updater removed — Google Code is defunct
+        }
+    }
+}
